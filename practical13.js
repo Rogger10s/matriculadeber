7f70a43fc873c3d3b7ceb56bf3881cbae57f3fe0
Rogger10s <nrouchaicelag@unl.edu.ec>
2016-07-22 01:45:06 -0500

    Update practical13.js

--- practical13.js
+++ practical13.js
@@ -1,15 +1,27 @@
 var app=angular.module('aps',['ngResource']);
 
-app.controller("contr", function($scope, list, list2){
+app.controller("contr", function($scope, list, list2, $window){
 	$scope.mensaje="jdhdjshd";
 	$scope.alum=list.get();
 	$scope.mate=list2.get();
-	
-	$scope.confirmar=function(){
-
-				window.location.href="confirmacion.html";
+	$scope.di=function(){
 		
-}
+		$window.location.href = 'confirmacion.html'
+	}
+	$scope.verificar=function(ced){
+		
+		for (var i = 0; i < $scope.alum.length; i++) {
+			if(angular.equals(ced, $scope.alum[i].cedula)){
+				alert("usted esta matriculado");
+				$window.location.href = 'practica13.html'
+			}
+			else{
+				alert("estudiante no existe");
+				$window.location.href = 'practica13.html'
+			}
+		}		
+	}
+
 });
 app.factory("list", function($resource){
 	//$http es un servicio de http
